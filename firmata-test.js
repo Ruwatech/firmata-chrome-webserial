0bd83f8845c17931dd774fa24588bd11b14a30b4
Ruwatech <ruwatech@live.com>
2022-10-30 21:00:19 +0530

    analog read

--- firmata-test.js
+++ firmata-test.js
@@ -2,7 +2,7 @@ let writer;
 let pin_io_array = new Uint8Array(39);
 let pin_direction_array = new Uint8Array(39);
 
-const analogPinsESP32 = {"39":0, "36":1, "35":2, "34":3, "33":4, "32":5, "27":6, "26":7, "25":8, "15":9, "14":10, "13":11, "12":12, "5":13, "4":14, "2":15};
+const analogPinsESP32 = { "39": 0, "36": 1, "35": 2, "34": 3, "33": 4, "32": 5, "27": 6, "26": 7, "25": 8, "15": 9, "14": 10, "13": 11, "12": 12, "5": 13, "4": 14, "2": 15 };
 
 
 async function connect_serial() {
@@ -16,7 +16,7 @@ async function connect_serial() {
     };
     //Try to connect to the Serial port
     try {
-        port = await navigator.serial.requestPort({filters: [filter1,filter2]});
+        port = await navigator.serial.requestPort({ filters: [filter1, filter2] });
         // Continue connecting to |port|.
 
         // - Wait for the port to open.
@@ -35,63 +35,63 @@ async function connect_serial() {
         //outputStream = encoder.writable;
         writer = port.writable.getWriter();
         reader = inputStream.getReader();
-      
+
     } catch (e) {
         //If the pipeTo error appears; clarify the problem by giving suggestions.
         if (e == "TypeError: Cannot read property 'pipeTo' of undefined") {
             e += "\n Use Google Chrome and enable-experimental-web-platform-features"
         }
-        
+
     }
 };
 
 async function writeToStream(line) {
     writer.write(line);
-   // console.log(line);
+    // console.log(line);
     //writer.releaseLock();
 };
 
-function digitalWrite_fmt(pin, state){
+function digitalWrite_fmt(pin, state) {
     var arrays = new Uint8Array(6);
-    let port_pin = Math.floor(pin/8);
+    let port_pin = Math.floor(pin / 8);
     arrays[0] = 0xF4;
     arrays[1] = pin;
     arrays[2] = 1;
-    
+
     pin_direction_array[pin] = 1;
 
     arrays[3] = 0x90 + port_pin;
     pin_io_array[pin] = state;
 
-    
+
     let portVal = 0;
     j = 0;
-    for(let i = port_pin*8; i<=pin+7; i++){
+    for (let i = port_pin * 8; i <= pin + 7; i++) {
         console.log(i);
-         if(pin_io_array[i] == 1){
-            portVal |= (1<<j);
-         }else{
-            portVal &= ~(1<<j);
-         }
-         j++;
+        if (pin_io_array[i] == 1) {
+            portVal |= (1 << j);
+        } else {
+            portVal &= ~(1 << j);
+        }
+        j++;
     }
     //console.log(portVal.toString(2));
     arrays[4] = portVal;
-    pin_io_array[(port_pin-1)*8+1]<<1 + pin_io_array[(port_pin-1)*8];
-    
+    pin_io_array[(port_pin - 1) * 8 + 1] << 1 + pin_io_array[(port_pin - 1) * 8];
+
     arrays[5] = 0x00;
 
-    writeToStream(arrays); 
+    writeToStream(arrays);
     //readLoop();
 }
 
-function analog_write_fm(port,value){
+function analog_write_fm(port, value) {
     let ared_val = 0;
 
-    return 
+    return
 }
 
-async function analog_read_fmt(pin){
+async function analog_read_fmt(pin) {
     let arrays = new Uint8Array(6);
     let pin_s = analogPinsESP32[pin];
     arrays[0] = 0xF4;
@@ -99,11 +99,9 @@ async function analog_read_fmt(pin){
     arrays[2] = 0x02;
     arrays[3] = pin_s;
     arrays[4] = 0x01;
-    writeToStream(arrays); 
-    let read_val = await readLoop().then((value) => {
-        console.log(value);
-        // expected output: "Success!"
-      });
+    writeToStream(arrays);
+    let read_val = await readLoop()
+    console.log(read_val.charCodeAt(1)+read_val.charCodeAt(2)<<7);
 }
 
 async function readLoop() {
@@ -111,18 +109,18 @@ async function readLoop() {
     let timeout_check_serial = Date.now();
     let val;
     while (true) {
-      const { value, done } = await reader.read();
-      //console.log(value);
-      if (done) {
-        // Allow the serial port to be closed later.
+        const { value, done } = await reader.read();
         //console.log(value);
-        val = value;
-        reader.releaseLock();
-        
-        break;
-      }
-      // value is a string.
+        console.log("initv:" + value);
+        console.log(value.length);
+        if (value.length >= 2) {
+            val = value;
+            //reader.releaseLock();
+            break;
+        }
 
+        
+    };
+   // console.log("read_v:" + val);
     return val;
-};
 }
